f17d5cc56b6c9756108cd01324a740a668727eed
wing.sun <wingsun.for.study@gmail.com>
2021-03-15 11:56:45 +0000

    delete user_controller index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,12 @@
 class UsersController < ApplicationController
   
- def index
- end
  
   def new
     @user = User.new
   end
 
   def create
-    @user = User.new(user_params)
+      @user = User.new(user_params)
     if @user.save
       flash[:success] = '登録が完了しました'
       redirect_to root_url
