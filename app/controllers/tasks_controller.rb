0d8ba7b08dc245df054e0789ae682f5de98e166b
wing.sun <wingsun.for.study@gmail.com>
2021-03-15 05:14:22 +0000

    add post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,17 @@
 class TasksController < ApplicationController
- before_action :require_user_logged_in, only: [:index,:show]
+ before_action :require_user_logged_in #, only: [:index,:show, :edit, :new]
+  before_action :correct_user, only: [:destroy]
   
   def index
-    @tasks = Task.all
+    if logged_in?
+      @task = current_user.tasks.build  
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    end
+
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = 'タスクが正常に登録されました'
@@ -23,11 +28,13 @@ class TasksController < ApplicationController
   end
   
   def edit
-    @task = Task.find(params[:id])
+   @task = current_user.tasks.find(params[:id])
+   #@task = Task.find(params[:id])
   end
   
   def show
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
+    #@task = Task.find(params[:id])
   end
   
   def update
@@ -43,11 +50,9 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    @task = Task.find(params[:id])
-    @task.destroy
-    
-    flash[:success] = 'タスクは正常に削除されました'
-    redirect_to tasks_url
+     @task.destroy
+    flash[:success] = 'タスクを削除しました。'
+    redirect_to root_url
   end
   
   private
@@ -56,4 +61,11 @@ class TasksController < ApplicationController
     params.require(:task).permit(:content, :status)
   end
   
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 end
